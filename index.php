028ef0e8fe02669ab9fd5aeaae8ae42dac4a8abc
Antony <91181127+Getsugo@users.noreply.github.com>
2022-01-25 09:40:39 +0100

    dbb
    dbb

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="yolo.css">
+</head>
+
+<body>
+
+
+        <h1>Ptit comptable</h1>
+
+        <button id="yolo">cc</button>
+        <form action="" method="get" class="form-creat-account">
+            <div class="form-account">
+                <label for="Name">Nom</label>
+                <input id="inputName"><br>
+                <label for="solde">Solde</label>
+                <input id="inputSolde"><br>
+                <label for="type">type</label>
+                <input id="inputType"><br>
+                <label for="devise">devise</label>
+                <input id="inputDevise"><br>
+                <label for="budget">budget</label>
+                <input id="inputBudget"><br>
+                <input type="submit" class="button" id="buttonValider">
+            </div>
+
+            <input type="submit" class="buttonsup" id="sup">
+
+        </form>
+</body>
+
+</html>
